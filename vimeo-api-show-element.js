c9aab553188e1b110c2ffdc6334cba535ec311ec
acasalsr <72133477+acasalsr@users.noreply.github.com>
2022-03-24 10:31:22 +0100

    vimeo-show-element

---
+++ vimeo-api-show-element.js
@@ -0,0 +1,18 @@
+<script src="https://player.vimeo.com/api/player.js"></script>;
+//<script>
+const iframe = document.querySelector(".vimeo-cc-desktop"); //.vimeo-cc-desktop -> modificar nom classe del embed de vimeo
+const player = new Vimeo.Player(iframe);
+var refreshIntervalId = setInterval(function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      if (seconds > 2270) {
+        // segons a modificar 10s, per exemple
+        $("#show-button").css("display", "inline"); // display: inline; -> s'ha d'ocultar el element que volem mostrar a None.
+        $("#show-button").show();
+        clearInterval(refreshIntervalId);
+      }
+    })
+    .catch(function (error) {});
+}, 1000);
+//</script>
